e5c76168ff7b40a1e16b5ec7811fc14ab87a0474
salahuddin07 <karimsalahuddin03@gmail.com>
2023-12-19 14:45:42 +0600

    read the todo and store in a text file

--- main.py
+++ main.py
@@ -1,27 +1,38 @@
-todos = []
-
-
 while True:
     user_action = input("enter add, show, edit, complete or exit: ")
     user_action = user_action.strip()
     match user_action:
         case 'add':
-            todo = input("Enter a todo: ")
+            todo = input("Enter a todo: ") + "\n"
+
+            file = open('todos.txt', 'r')
+            todos = file.readlines()
+            file.close()
+
             todos.append(todo)
+            file = open('todos.txt', 'w')
+            file.writelines(todos)
+            file.close()
+
         case 'show':
+            file = open('todos.txt', 'r')
+            todos = file.readlines()
+            file.close()
+
             for index, item in enumerate(todos):
-                row = f"{index+1} - {item}"
+                item = item.strip('\n')
+                row = f"{index+1}-{item}"
                 print(row)
 
         case 'edit':
             number = int(input('number of the todo to edit: '))
-            index_number = number - 1
+            index_number = number-1
             new_todo = input('enter new todo: ')
             todos[index_number] = new_todo
 
         case 'complete':
             number = int(input('number of the todo to complete: '))
-            todos.pop(number - 1 )
+            todos.pop(number-1)
 
         case 'exit':
             break
